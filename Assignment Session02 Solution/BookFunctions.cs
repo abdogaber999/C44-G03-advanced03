dfdbf33b5dc23a3352e5aa54ceb2c2008e21be1c
abdogaber999 <153696740+abdogaber999@users.noreply.github.com>
2025-09-09 09:05:38 +0300

    Add project files.

---
+++ Assignment Session02 Solution/BookFunctions.cs
@@ -0,0 +1,30 @@
+﻿using Assignment_Session02_Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate string BookFunctionDelegate(Book B);
+
+
+namespace Assignment_Session02_Solution
+{
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+        public static string GetAuthors(Book B)
+        {
+            return string.Join(", ", B.Authors);
+        }
+
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString();
+        }
+    }
+}
---
+++ Assignment Session02 Solution/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_Session02_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+        {
+            new Book("1", "C# Basics", new string[]{"Abdo", "Gaber"}, new DateTime(2020, 3, 1), 150),
+            new Book("2", "OOP Concepts", new string[]{"Mohamed"}, new DateTime(2021, 4, 15), 200)
+        };
+
+            Console.WriteLine("=== Using User-defined Delegate ===");
+            LibraryEngine.ProcessBooks(books, BookFunctions.GetTitle);
+
+            Console.WriteLine("\n=== Using BCL Delegate (Func<>) ===");
+            LibraryEngine.ProcessBooks(books, new BookFunctionDelegate(BookFunctions.GetAuthors));
+
+            Console.WriteLine("\n=== Using Anonymous Method (GetISBN) ===");
+            LibraryEngine.ProcessBooks(books, delegate (Book b) { return b.ISBN; });
+
+            Console.WriteLine("\n=== Using Lambda Expression (GetPublicationDate) ===");
+            LibraryEngine.ProcessBooks(books, (b) => b.PublicationDate.ToShortDateString());
+        }
+    }
+}
